fb27f994e6a6a2fb25b1365374c6374df3fcc125
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-05-12 13:39:34 -0500

    comienzo codigo triangulo

--- figuras.js
+++ figuras.js
@@ -1,8 +1,22 @@
+//Codigo del cuadrado
 const ladoCuadrado = 5;
-console.log("Los lados del cuadrado miden: " + ladoCuadrado);
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
 
 const perimetroCuadrado = ladoCuadrado * 4;
-console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("El area del cuadrado es: " + areaCuadrado);
+console.log("El area del cuadrado es: " + areaCuadrado + "cm2");
+
+//Codigo del triangulo
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+console.log("Los lados del triangulo miden: " 
+  + ladoTriangulo1 
+  + " cm, " 
+  + ladoTriangulo2 
+  + " cm, " 
+  + baseTriangulo 
+  + " cm."
+);
